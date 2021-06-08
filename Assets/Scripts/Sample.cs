58b0b357024da198f74a6519a21ff3e08ff69643
amin khosravi <siavashmehmandoost@gmail.com>
2021-06-08 11:08:07 +0430

    2ndCommit

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Sample : MonoBehaviour
 {
+    public InputField username;
+    public InputField password;
+    public int score;
+    public int level;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,24 @@ public class Sample : MonoBehaviour
     void Update()
     {
         
+    }
+    IEnumerator Login()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Username",username.text);
+        form.AddField("Password",password.text);
+        UnityWebRequest request = UnityWebRequest.Post("",form);
+        yield return request.SendWebRequest();
+
+        if(request.downloadHandler.text[0]=='0')
+        {
+            Debug.Log("Log in successfully");
+            score = int.Parse(request.downloadHandler.text.Split('\t')[1]);
+            level = int.Parse(request.downloadHandler.text.Split('\t')[2]);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
     }
 }
