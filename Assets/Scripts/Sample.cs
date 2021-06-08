274b1d1fc800d7fa04db0c91eccb1664006100f4
amin khosravi <siavashmehmandoost@gmail.com>
2021-06-08 11:32:49 +0430

    3rdCommit

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Sample : MonoBehaviour
 {
+    public NavMeshAgent agent;
     public InputField username;
     public InputField password;
     public int score;
@@ -13,12 +15,26 @@ public class Sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        City city1 = new City(2000,"Birjand","Iran");
+        City city2 = new City(1000000,"NewYork","USA");
+        city1.MyCity();
+        city2.MyCity();
+        agent = GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,100))
+            {
+                agent.destination = hit.point;
+            }
+        }
         
     }
     IEnumerator Login()
@@ -38,6 +54,23 @@ public class Sample : MonoBehaviour
         else
         {
             Debug.Log(request.downloadHandler.text);
+        }
+    }
+    public class City
+    {
+        public int population;
+        public string name;
+        public string country;
+
+        public City(int mypopulation, string myname, string mycountry)
+        {
+            population = mypopulation;
+            name = myname;
+            country = mycountry;
+        }
+        public void MyCity()
+        {
+            Debug.Log($"{name} is a city in {country} with {population}");
         }
     }
 }
