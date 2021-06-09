48aed2756da4c961e77ba24e51f2078a55c189d5
amin khosravi <siavashmehmandoost@gmail.com>
2021-06-09 20:35:09 +0430

    5thCommit

---
+++ Assets/Scripts/AssetBundleCreate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleCreate : Editor
+{
+    [MenuItem("Assets/Create AssetBundle")]
+    static void CreateAssetBundle()
+    {
+        BuildPipeline.BuildAssetBundles(@"C:\Users\amink\Desktop\AssetBundle",BuildAssetBundleOptions.ChunkBasedCompression,BuildTarget.StandaloneWindows64);
+    }
+}
